7e457189a4995b3173ad1dc9adf15bdbd088e258
jglee <jinkyu0826@gmail.com>
2022-11-24 13:19:08 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 112 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, k;
+  cin >> n >> k;
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  int mx = 0;
+  int len = 0;
+  int dCnt = 0;
+  int j = 0;
+  for (int i = 0; i < n; i++) {
+    while (j < n) {
+      if (arr[j] % 2 == 1 && dCnt == k) break;
+      if (arr[j] % 2 == 1) {
+        dCnt++;
+      }
+      j++;
+    }
+    len = j - i - dCnt;
+    mx = max(mx, len);
+    if (arr[i] % 2 == 1 && dCnt > 0) dCnt--;
+  }
+
+  cout << mx;
+}
