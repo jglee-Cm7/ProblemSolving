0b023f695db039ab2e0d32665b10c08ba9535fbb
jglee <jinkyu0826@gmail.com>
2022-11-28 16:10:05 +0900

    [Gold II] Title: 보석 도둑, Time: 312 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+  if (a.second == b.second)
+    return a.first < b.first;
+  else
+    return a.second > b.second;
+};
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, k;
+  cin >> n >> k;
+
+  vector<pair<int, int>> gem(n);
+  multiset<int> bag;
+
+  for (int i = 0; i < n; i++) {
+    int m, v;
+    cin >> m >> v;
+    gem[i] = {m, v};
+  }
+  for (int i = 0; i < k; i++) {
+    int b;
+    cin >> b;
+    bag.insert(b);
+  }
+
+  sort(gem.begin(), gem.end(), cmp);
+
+  long long ans = 0;
+  for (auto g : gem) {
+    auto it = bag.lower_bound(g.first);
+    if (it != bag.end()) {
+      ans += g.second;
+      bag.erase(it);
+    }
+  }
+
+  cout << ans;
+}
