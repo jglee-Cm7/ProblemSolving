7b6d3b0fb0ff1a4d90d4b66de29195acdd1aa0f1
jglee <jinkyu0826@gmail.com>
2022-11-30 12:37:27 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 56 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.cc
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.cc
@@ -8,11 +8,6 @@ bool check[605];
 int makeSnow(int ans, int s1) {
   int mn = ans;
   int s2 = 0;
-
-  // for (int i = 0; i < n; i++)
-  //   cout << check[i] << " ";
-  // cout << "ans: " << ans << " | s1: " << s1 << "\n";
-
   int y = n - 1;
   for (int x = 0; x < n; x++) {
     if (mn == 0) break;
@@ -20,15 +15,11 @@ int makeSnow(int ans, int s1) {
     if (check[x]) continue;
     while (check[y])
       y--;
-    // cout << x << " " << y << "\n";
     s2 = h[x] + h[y];
     mn = min(mn, abs(s2 - s1));
     while (s1 < s2) {
       y--;
-      if (x == y) {
-        return mn;
-        cout << s1 << "\n";
-      }
+      if (x == y) return mn;
       if (check[y]) continue;
       s2 = h[x] + h[y];
       mn = min(mn, abs(s2 - s1));
