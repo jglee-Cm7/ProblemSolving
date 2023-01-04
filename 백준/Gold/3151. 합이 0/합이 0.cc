65fc5d916e1ae8ed4e1aeff52dafbdba55898f7f
jglee <jinkyu0826@gmail.com>
2023-01-04 11:16:38 +0900

    [Gold IV] Title: 합이 0, Time: 1744 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  vector<int> std;
+  for (int i = 0; i < n; i++) {
+    int a;
+    cin >> a;
+    std.push_back(a);
+  }
+
+  sort(std.begin(), std.end());
+  // for (int s : std)
+  //   cout << s << " ";
+  // cout << "\n";
+
+  long long ans = 0;
+  for (int i = 0; i < n; i++) {
+    int s1 = std[i];
+    if (s1 > 0) break;
+    for (int j = i + 1; j < n; j++) {
+      int s2 = std[j];
+      long long count =
+          upper_bound(std.begin() + j + 1, std.end(), -(s1 + s2)) -
+          lower_bound(std.begin() + j + 1, std.end(), -(s1 + s2));
+
+      // if (count)
+      //   cout << s1 << " " << s2 << " " << -(s1 + s2) << " : " << count <<
+      //   "\n";
+      ans += count;
+    }
+  }
+  cout << ans;
+}
