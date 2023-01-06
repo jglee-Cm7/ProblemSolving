8f3371fca5d6fb528a98cb732a49325673b5c1a6
jglee <jinkyu0826@gmail.com>
2023-01-06 14:17:56 +0900

    [Gold IV] Title: 운동, Time: 100 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.cc
+++ 백준/Gold/1956. 운동/운동.cc
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
+#define INF 0x3f3f3f3f
 using namespace std;
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   int v, e;
   cin >> v >> e;
 
-  vector<vector<int>> floyd(v + 1, vector<int>(v + 1, 0x3f3f3f3f));
+  vector<vector<int>> floyd(v + 1, vector<int>(v + 1, INF));
   while (e--) {
     int a, b, c;
     cin >> a >> b >> c;
-    floyd[a][b] = min(floyd[a][b], c);
+    floyd[a][b] = c;
   }
   for (int k = 1; k <= v; k++) {
     for (int i = 1; i <= v; i++) {
@@ -19,18 +21,10 @@ int main() {
     }
   }
 
-  // for (int i = 1; i <= v; i++) {
-  //   for (int j = 1; j <= v; j++)
-  //     cout << floyd[i][j] << " ";
-  //   cout << "\n";
-  // }
-
-  int ans = 0x3f3f3f3f;
+  int ans = INF;
   for (int i = 1; i <= v; i++)
     ans = min(ans, floyd[i][i]);
 
-  if (ans >= 0x3f3f3f3f)
-    cout << "-1";
-  else
-    cout << ans;
+  if (ans == INF) ans = -1;
+  cout << ans;
 }
