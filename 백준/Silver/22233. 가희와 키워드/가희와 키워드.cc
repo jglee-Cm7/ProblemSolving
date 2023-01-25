1d2a18604353fe880cbc09a602789d2a77d8b99d
jglee <jinkyu0826@gmail.com>
2023-01-25 22:09:54 +0900

    [Silver II] Title: 가희와 키워드, Time: 1396 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, m;
+  cin >> n >> m;
+  set<string> note;
+  while (n--) {
+    string s;
+    cin >> s;
+    note.insert(s);
+  }
+  while (m--) {
+    string l;
+    cin >> l;
+    string keyword;
+    int curPos = 0;
+    int pos;
+    while ((pos = l.find(",", curPos)) != string::npos) {
+      int length = pos - curPos;
+      keyword = l.substr(curPos, length);
+      note.erase(keyword);
+      curPos = pos + 1;
+    }
+    keyword = l.substr(curPos);
+    note.erase(keyword);
+    cout << note.size() << "\n";
+  }
+}
