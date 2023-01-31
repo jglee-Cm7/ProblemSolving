28da318f633aac8e4e36800434166cf48152d595
jglee <jinkyu0826@gmail.com>
2023-02-01 00:48:28 +0900

    [Gold II] Title: 불켜기, Time: 228 ms, Memory: 2656 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.cc
+++ 백준/Gold/11967. 불켜기/불켜기.cc
@@ -17,41 +17,29 @@ int main() {
     adj[(x - 1) * n + y - 1].push_back((a - 1) * n + b - 1);
   }
 
-  // for (int i = 0; i < n * n; i++) {
-  //   cout << i << " : ";
-  //   for (int aa : adj[i])
-  //     cout << aa << " ";
-  //   cout << "\n";
-  // }
-
-  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
+  queue<int> q;
 
   int ans = 1;
   bool exit = true;
   while (1) {
     exit = true;
     fill(vis.begin(), vis.end(), 0);
-    q.push({0, 0});
+    q.push(0);
     vis[0] = 1;
     light[0] = 0;
     while (!q.empty()) {
-      auto [l, cur] = q.top();
+      auto cur = q.front();
       q.pop();
-      if (cur == 0) {
-      }
       if (light[cur] == 1) continue;
-      // cout << cur << " visit! : ";
       int x = cur / n;
       int y = cur % n;
       for (int nxt : adj[cur]) {
         if (light[nxt] == 1) {
           light[nxt] = 0;
-          // cout << nxt << " ";
           ans++;
           exit = false;
         }
       }
-      // cout << "\n";
       for (int dir = 0; dir < 4; dir++) {
         int nx = x + dx[dir];
         int ny = y + dy[dir];
@@ -59,7 +47,7 @@ int main() {
         if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
         if (vis[nxt]) continue;
         vis[nxt] = 1;
-        q.push({light[nxt], nxt});
+        q.push(nxt);
       }
     }
     if (exit) break;
