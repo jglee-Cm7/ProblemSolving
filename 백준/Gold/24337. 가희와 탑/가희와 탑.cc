a27c4fa44733797c51e3cb03d27573f4dd7c361a
jglee <jinkyu0826@gmail.com>
2023-02-10 15:21:14 +0900

    [Gold III] Title: 가희와 탑, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/24337. 가희와 탑/가희와 탑.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, a, b;
+  cin >> n >> a >> b;
+  vector<int> ans;
+  for (int i = 1; i < a; i++)
+    ans.push_back(i);
+  ans.push_back(max(a, b));
+  for (int i = b - 1; i > 0; i--)
+    ans.push_back(i);
+
+  if ((int)ans.size() > n) {
+    cout << "-1";
+    return 0;
+  } else {
+    cout << ans[0] << " ";
+    for (int i = 0; i < n - (int)ans.size(); i++)
+      cout << "1 ";
+    for (int i = 1; i < (int)ans.size(); i++)
+      cout << ans[i] << " ";
+  }
+}
