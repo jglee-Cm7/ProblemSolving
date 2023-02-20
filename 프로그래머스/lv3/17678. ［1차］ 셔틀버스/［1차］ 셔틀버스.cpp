9d93563ac4a2fa91629105f60376f1ed5787579f
jglee <jinkyu0826@gmail.com>
2023-02-20 15:07:02 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 0.20 ms, Memory: 3.9 MB -BaekjoonHub

--- 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
+++ 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
@@ -15,28 +15,20 @@ string solution(int n, int t, int m, vector<string> timetable) {
   for (int t : tt)
     q.push(t);
 
-  // for (int t : tt)
-  //   cout << t << "\n";
-  // cout << "\n";
-
   int st = 540 - t;
   vector<int> ps;
   for (int i = 0; i < n; i++) {
     st += t;
-    // cout << st << " : ";
     for (int j = 0; j < m; j++) {
       if (!q.empty() && q.front() <= st) {
         if (i == n - 1) ps.push_back(q.front());
-        // cout << q.front() << " ";
         q.pop();
       }
     }
-    // cout << "\n";
   }
+
   int ans = st;
-  if (ps.size() > 0 && ps.size() < m && ps[ps.size() - 1] >= 540)
-    ans = ps[ps.size() - 1] - 1;
-  else if (ps.size() == m)
+  if ((ps.size() > 0 && ps[ps.size() - 1] >= 540) || ps.size() == m)
     ans = ps[ps.size() - 1] - 1;
   int hour = ans / 60;
   int minute = ans % 60;
