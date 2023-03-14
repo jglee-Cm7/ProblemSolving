012f093d52be3625a5277b346fe44901da8b3d86
jglee <jinkyu0826@gmail.com>
2023-03-14 22:43:07 +0900

    [Unrated] Title: 동물원, Time: 8 ms, Memory: 7532 KB -BaekjoonHub

--- 백준/Unrated/1309. 동물원/동물원.cc
+++ 백준/Unrated/1309. 동물원/동물원.cc
@@ -7,8 +7,8 @@ int main() {
   cin >> n;
   vector<vector<int>> dp(n + 1, vector<int>(3, 0));
   dp[0][0] = 1;
-  for (int i = 1; i < n + 1; i++) {
-    dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % 9901 + dp[i - 1][2] % 9901;
+  for (int i = 1; i <= n; i++) {
+    dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
     dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
     dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
   }
@@ -16,7 +16,3 @@ int main() {
   int ans = (dp[n][0] + dp[n][1] + dp[n][2]) % 9901;
   cout << ans;
 }
-
-/*
-  DP
-*/
