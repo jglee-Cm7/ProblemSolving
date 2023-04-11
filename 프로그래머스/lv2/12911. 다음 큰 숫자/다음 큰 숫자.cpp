5913563c37a3dde44a7ba39b5d9d633ee10dcef4
jglee <jinkyu0826@gmail.com>
2023-04-12 01:40:26 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.55 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -4,15 +4,10 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    bitset<20> p(n);
-    int cnt = p.count();
+    int cnt = bitset<20>(n).count();
     
-    for(int i=n+1; i<= 1000000; i++) {
-        bitset<20> b(i);
-        if(b.count() == cnt) {
-            answer = b.to_ulong();
-            break;
-        }
-    }
+    for(int i=n+1; i<= 1000000; i++)
+        if(bitset<20>(i).count() == cnt) return i;
+
     return answer;
 }
