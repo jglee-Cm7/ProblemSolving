ffbae851957352305a5bf609627d273ef625be6e
jglee <jinkyu0826@gmail.com>
2023-04-27 20:23:53 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+map<string, vector<pair<string, bool>>> adj;
+
+void func(int k, string st, vector<string>& answer) {
+    if(k == n) {
+        for(string ans : answer)
+            cout << ans << " ";
+        cout << "\n";
+        return;
+    }
+    
+    for(auto& [en, used] : adj[st]) {
+        if(used) continue;
+        answer.push_back(en);
+        used = true;
+        func(k+1, en, answer);
+        if(answer.size() == n+1) return;
+        answer.pop_back();
+        used = false;
+    }
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+    vector<string> answer;
+    n = tickets.size();
+    for(auto ticket: tickets)
+        adj[ticket[0]].push_back({ticket[1], 0});
+    
+    for(auto& [key, value] : adj)
+        sort(value.begin(), value.end());
+    
+    answer.push_back("ICN");
+    func(0, "ICN", answer);
+    
+    return answer;
+}
