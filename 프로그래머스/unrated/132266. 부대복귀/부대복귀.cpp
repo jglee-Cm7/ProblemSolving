9461ecef1f5126457c218970319597e6474f5fdb
jglee <jinkyu0826@gmail.com>
2023-06-24 11:35:05 +0900

    [unrated] Title: 부대복귀, Time: 31.84 ms, Memory: 27.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132266. 부대복귀/부대복귀.cpp
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.cpp
@@ -1,38 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> adj[100010];
+vector<pair<int,int>> adj[100010];
 
 vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
     vector<int> answer;
     for(auto r : roads) {
         int u = r[0], v = r[1];
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u].push_back({1, v});
+        adj[v].push_back({1, u});
     }
+    for(int i=1; i<=n; i++)
+        sort(adj[i].begin(), adj[i].end());
     
-    for(int src : sources) {
-        vector<int> vis(n+1, 0);
-        vis[src] = 1;
-        queue<int> q;
-        q.push(src);
-        bool find = false;
-        
-        while(!q.empty()) {
-            int cur = q.front();
-            if(cur == destination) {
-                answer.push_back(vis[cur]-1);
-                find = true;
-                break;
-            }
-            q.pop();
-            for(int nxt : adj[cur]) {
-                if(vis[nxt]) continue;
-                vis[nxt] = vis[cur]+1;
-                q.push(nxt);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+    vector<int> d(n+1, INT_MAX);
+    d[destination] = 0;
+    pq.push({0, destination});
+    while(!pq.empty()) {
+        auto [w, cur] = pq.top();
+        pq.pop();
+        if(d[cur] != w) continue;
+        for(auto [nw, nxt]: adj[cur]) {
+            if(w + nw < d[nxt]) {
+                d[nxt] = w + nw;
+                pq.push({d[nxt], nxt});
             }
         }
-        if(!find) answer.push_back(-1);
     }
+    for(int src : sources) {
+        if(d[src] == INT_MAX) answer.push_back(-1);
+        else answer.push_back(d[src]);
+    }
+        
     return answer;
 }
